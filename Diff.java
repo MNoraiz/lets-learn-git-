62bb19c7e579ebc2cde6b56cfa4b18c58427d8ce
Maher Noraiz <noraizmaher@gmail.com>
2022-06-20 20:11:17 +0400

    3rd

---
+++ Diff.java
@@ -0,0 +1,11 @@
+public class Diff {
+    public static void main(String[]arg){
+
+        int a =  5;
+        int b = 6;
+        
+        System.out.println("The product is " + (a-b));
+        System.out.println("The product is " + (a+b));
+
+    }
+}
